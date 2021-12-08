eeb29940773f2c1934006b773778914a6d4e541b
Kirill Alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2021-12-08 23:09:04 +0300

    fix: naming

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { showLoader, hideLoader } from "./src/scripts/UI/loader.js";
-import { $body, setMarginForHeader } from "./src/scripts/indent.js";
-import { setPhoneMask } from "./src/scripts/phones/inputMask.js";
-import { rewriteCallLinks } from "./src/scripts/phones/callLinks.js";
-import { navs, addHandlerPageNav } from "./src/scripts/move/navMove.js";
-import { applications, moveToSendSection } from "./src/scripts/move/toSendSection.js";
-import { $burgerIcon, toggleShowMenu } from "./src/scripts/burger.js";
+import { showLoader, hideLoader } from "./src/scripts/UI/loader.js"
+import { $body, setMarginForHeader } from "./src/scripts/indent.js"
+import { setPhoneMask } from "./src/scripts/phones/inputMask.js"
+import { rewriteCallLinks } from "./src/scripts/phones/callLinks.js"
+import { navs, addHandlerPageNav } from "./src/scripts/move/navMove.js"
+import { applications, moveToSendSection } from "./src/scripts/move/toSendSection.js"
+import { $burgerIcon, toggleShowMenu } from "./src/scripts/burger.js"
 import {
   $slider,
   $sliderNav,
@@ -13,38 +13,36 @@ import {
   setPositionTouchEnd,
   addHandlerSliderTouch,
   addHandlerSliderClicks,
-} from "./src/scripts/slider/swiper.js";
-import { startAutoSwiper } from "./src/scripts/slider/autoSwiper.js";
-import { $sendForm, handlerChangeSendForm } from "./src/scripts/form/validator.js";
-import { submitForm } from "./src/scripts/form/submit.js";
+} from "./src/scripts/slider/swiper.js"
+import { startAutoSwiper } from "./src/scripts/slider/autoSwiper.js"
+import { $sendForm, handlerChangeSendForm } from "./src/scripts/form/validator.js"
+import { submitForm } from "./src/scripts/form/submit.js"
 
 const start = () => {
   try {
-    setMarginForHeader();
-    setPhoneMask();
-    rewriteCallLinks();
-    setPoginationSize();
-    startAutoSwiper();
+    setMarginForHeader()
+    setPhoneMask()
+    rewriteCallLinks()
+    setPoginationSize()
+    startAutoSwiper()
 
-    navs.forEach(($nav) => $nav.addEventListener("click", addHandlerPageNav));
-    applications.forEach(($application) =>
-      $application.addEventListener("click", moveToSendSection)
-    );
-    $burgerIcon.addEventListener("click", toggleShowMenu);
-    $slider.addEventListener("touchstart", setPositionTouchStart);
-    $slider.addEventListener("touchmove", setPositionTouchEnd);
-    $slider.addEventListener("touchend", addHandlerSliderTouch);
-    $sliderNav.addEventListener("click", addHandlerSliderClicks);
-    $sendForm.addEventListener("change", handlerChangeSendForm);
-    $sendForm.addEventListener("submit", submitForm);
+    navs.forEach(($nav) => $nav.addEventListener("click", addHandlerPageNav))
+    applications.forEach(($application) => $application.addEventListener("click", moveToSendSection))
+    $burgerIcon.addEventListener("click", toggleShowMenu)
+    $slider.addEventListener("touchstart", setPositionTouchStart)
+    $slider.addEventListener("touchmove", setPositionTouchEnd)
+    $slider.addEventListener("touchend", addHandlerSliderTouch)
+    $sliderNav.addEventListener("click", addHandlerSliderClicks)
+    $sendForm.addEventListener("change", handlerChangeSendForm)
+    $sendForm.addEventListener("submit", submitForm)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   } finally {
-    $body.style.opacity = 1;
+    $body.style.opacity = 1
 
-    hideLoader();
+    hideLoader()
   }
-};
+}
 
-window.addEventListener("DOMContentLoaded", showLoader);
-window.addEventListener("load", start);
+window.addEventListener("DOMContentLoaded", showLoader)
+window.addEventListener("load", start)
